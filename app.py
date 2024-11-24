84877de5f46631fb3f4fab139967b71dd07a675d
tanis <tk009@lvc.edu>
2024-11-24 00:46:39 -0500

    Dependencies imported

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import customtkinter as ck
+
+import pandas as pd
+import numpy as np
+import pickle
+
+import mediapipe as mp
+import cv2
+from PIL import Image, ImageTk
+
+from landmarks import landmarks
