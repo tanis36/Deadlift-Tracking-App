2971a781b315379b6ad7450cbc41b2347710f08e
tanis <tk009@lvc.edu>
2024-11-25 19:31:55 -0500

    Window now displays class, counter, and probability labels and boxes

--- app.py
+++ app.py
@@ -16,4 +16,28 @@ window.geometry("480x700")
 window.title("Deadlift Tracker")
 ck.set_appearance_mode("dark")
 
+class_label = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="black", padx=10)
+class_label.place(x=10, y=1)
+class_label.configure(text='STAGE')
+
+counter_label = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="black", padx=10)
+counter_label.place(x=180, y=1)
+counter_label.configure(text='REPS')
+
+prob_label = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="black", padx=10)
+prob_label.place(x=350, y=1)
+prob_label.configure(text='PROB')
+
+class_box = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="white", fg_color="blue")
+class_box.place(x=10, y=41)
+class_box.configure(text='0')
+
+counter_box = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="white", fg_color="blue")
+counter_box.place(x=180, y=41)
+counter_box.configure(text='0')
+
+prob_box = ck.CTkLabel(window, height=40, width=120, font=("Arial", 20), text_color="white", fg_color="blue")
+prob_box.place(x=350, y=41)
+prob_box.configure(text='0')
+
 window.mainloop()
