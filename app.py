3d8d2684a717b6c02a4de8b3ab72a3e7df48a7cf
tanis <tk009@lvc.edu>
2024-11-25 19:05:33 -0500

    Created display window

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ import cv2
 from PIL import Image, ImageTk
 
 from landmarks import landmarks
+
+window = tk.Tk()
+window.geometry("480x700")
+window.title("Deadlift Tracker")
+ck.set_appearance_mode("dark")
+
+window.mainloop()
